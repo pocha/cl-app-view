254957c81f11aba163b7d030c62e11544d0dbc21
pocha <pocha@stalkninja.com>
2013-01-05 12:43:00 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CodelearnCourseView::Application.config.secret_token = '345b4f98328550562e141dbc918c542201035371646c3204075f4865b3c20b3ec86756c34c0c1cc31d0bc12b7a08d58bf75d33f3f0b30cbb954f41c9d6e46dd6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CodelearnCourseView::Application.config.session_store :cookie_store, key: '_codelearn_course_view_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CodelearnCourseView::Application.config.session_store :active_record_store
