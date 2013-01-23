cf2b94a0f2431f57dc68c8babbf9db2fb6a548ac
pocha <pocha@stalkninja.com>
2013-01-23 06:13:59 +0530

    fixed course view app to show content. Got broken because of changes at the content level

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,24 +1,15 @@
 class AppsController < ApplicationController
 
 	def new_layout
-	
+
 		if !params[:module_number].nil? and !params[:lesson_number].nil?
 			@current_module = LessonCollection.find_by_number(params[:module_number]) || not_found
 			@current_lesson = @current_module.lessons.find_by_number(params[:lesson_number]) || not_found
-			
-
-			if !params[:task_number].nil?
-				@current_task = @current_lesson.tasks.find_by_number(params[:task_number]) || not_found
-
-				@module_no = params[:module_number]
-				@lesson_no = params[:lesson_number]
-				@task_no = params[:task_number]
-				@title = @current_task.name.gsub("-", " ") + ' | Ruby on Rails Tutorial | Codelearn'
-				render 'new_layout_task', :layout => 'new_layout'  and return
-			else
-				@title = @current_lesson.name.gsub("-", " ") + ' | Ruby on Rails Tutorial | Codelearn'
-				render 'new_layout_lesson', :layout => 'new_layout'  and return
-			end
+			@module_no = params[:module_number]
+			@lesson_no = params[:lesson_number]
+			@task_no = params[:task_number]
+			@title = @current_lesson.name.gsub("-", " ") + ' | Ruby on Rails Tutorial | Codelearn'
+			render 'new_layout_lesson', :layout => 'new_layout'  and return
 		else
 			@title = 'Ruby on Rails Tutorial | Codelearn'
 			render 'new_layout_initial', :layout => 'new_layout'  and return
